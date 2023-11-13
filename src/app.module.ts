7e8ea366de56793bddd43f2c35ef639ac70f7b11
杨申 <15827304619@xtransfer.cn>
2023-11-13 19:08:48 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/models/user.module';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'believe1314.',
+      database: 'water-drop',
+      entities: [`${__dirname}/../modules/**/*.entity{.ts,.js}`],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
