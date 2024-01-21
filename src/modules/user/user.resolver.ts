82dd097d42e9721e9e1d6bfa2820024b2cf9657a
yang-shen-study <yangshen@192.168.0.100>
2024-01-21 15:37:05 +0800

    test

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -2,7 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { UserInput } from '../dto/user-input.type';
 import { UserType } from '../dto/user.type';
-
+// graphql层
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
